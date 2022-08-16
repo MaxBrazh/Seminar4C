51d5ea263dbc7a2d26e90726e6a606d802e460b4
Maksim <brazhmax@mail.ru>
2022-08-16 21:06:34 +0300

    add Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+using static System.Console;
+Clear();
+
+Write("Введите число a: ");
+int numberA = int.Parse(ReadLine());
+Write("Введите число b: ");
+int numberB = int.Parse(ReadLine());
+int step = MyPow(numberA, numberB);
+Write($"Число а в натуральной степени числа b = {step}");
+
+
+int MyPow(int a, int n)
+{
+int myresult = 1;
+for (int i = 0; i<n; i++)
+myresult *= a;
+return myresult;
+}
