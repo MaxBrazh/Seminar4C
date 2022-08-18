ad09d81c9d453c5093f4d67e914ec245b4b4d651
Maksim <brazhmax@mail.ru>
2022-08-18 10:35:29 +0300

    initial commit

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,30 +1,46 @@
 ﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
 using static System.Console;
 Clear();
 
-Write("Введите число");
-string numberAstr = Convert.ToString(ReadLine());
- 
-int[] array = GetArrayFromString(numberAstr); 
-result = SummArray(array);
-Write($"Сумма цифр введенног числа = {result}");
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
 
+int result = SummArray(number);
+WriteLine($"Сумма цифр введенного числа = {result}");
 
-int SummArray(int[] array)
+int SummArray(int number)
 {
-int n = array.Length;
-int result = 0;
-for (int i = 0; i < n-1; i++)
-    result = result + array[i];
-return result;
+    int razryad = digitCount(number);
+    int n = razryad;
+    int result = 0;
+    int i = 1;
+    while (i <= n)
+    {
+        result = result + (number %  MyPow(10, razryad)) / MyPow(10, razryad-1);
+        razryad--;
+        i++;
+    }
+    return result;
 }
 
 
-int[] GetArrayFromString(string arrayString)
-{     
-    string[] massString = arrayString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-    int[] result= new int [massString.Length];
-    for (int i=0; i<result.Length; i++)
-        result[i] = int.Parse(massString[i]);  
-    return result; 
+int digitCount(int number)
+{
+    int i = 0;
+    while (number != 0)
+    {
+        number /= 10;  
+        i++;
+    }
+    return i;
+}
+
+
+int MyPow(int a, int n)
+{
+    int myresult = 1;
+    for (int i = 0; i < n; i++)
+    myresult *= a;
+    return myresult;
 }
--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,24 +2,24 @@
 
 using static System.Console;
 Clear();
+
 string arrayString = ReadLine();
 string[] massString = arrayString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 if (massString.Length == 8)
 {
-int[] array = GetArrayFromString(massString); 
-WriteLine($"[{String.Join(",",array)}]");  
+    int[] array = GetArrayFromString(massString); 
+    WriteLine($"[{String.Join(",", array)}]");  
 }
 else
 WriteLine("Длина массива не равна 8!");
 
 
-
 int[] GetArrayFromString(string[] massString) 
 {     
-int[] result= new int [massString.Length];   
-for (int i = 0; i < result.Length; i++)     
-{         
-    result[i] = int.Parse(massString[i]);     
-}     
+    int[] result= new int [massString.Length];   
+    for (int i = 0; i < result.Length; i++)     
+    {         
+        result[i] = int.Parse(massString[i]);     
+    }     
 return result; 
 }
